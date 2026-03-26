c9689e92670a1e7a0f36f9d851eaf950f901b25c
admin@just-ai.com <admin@just-ai.com>
2026-03-26 09:13:43 +0000

    Scenario changes

---
+++ src/collections/Amo_Crm/Amo_Crm.toolspec.js
@@ -0,0 +1,13 @@
+import functions from './Amo_Crm.js';
+
+export default {
+  add_company_to_crm__f24aad6b: {
+    description: "Adds a new company to amo CRM",
+    parameters: {
+      "type" : "object",
+      "properties" : { },
+      "required" : [ ]
+    },
+    function: functions.add_company_to_crm__f24aad6b
+  },
+};
