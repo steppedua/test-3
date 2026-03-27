5be23f9dc051de60b8dab75502b4cccebd887ec2
admin@just-ai.com <admin@just-ai.com>
2026-03-27 10:53:14 +0000

    Apply template: adding-company-to-crm-ru

--- src/collections/Email/Email.js
+++
@@ -1,9 +0,0 @@
-async function send__bc368a0b(params) {
-    params.smtpIntegration = "null"
-    params.subject = "Результат скрининга"
-    return await Email.send(params)
-}
-
-export default {
-  send__bc368a0b
-}
--- src/collections/Rag/Rag.js
+++
@@ -1,8 +0,0 @@
-async function retrieveChunks__e2a5e57f(params) {
-    params.ragIntegrationKey = ""
-    return await Rag.retrieveChunks(params)
-}
-
-export default {
-  retrieveChunks__e2a5e57f
-}
