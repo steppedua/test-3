964e3c4f1cbf5a9159a09444586d87b942e49a13
admin@just-ai.com <admin@just-ai.com>
2026-03-27 10:55:43 +0000

    Scenario changes

---
+++ src/collections/Amo_Crm/Amo_Crm.js
@@ -0,0 +1,8 @@
+async function add_company_to_crm__f24aad6b(params) {
+    params.name = "null"
+    return await Amo_Crm.add_company_to_crm(params)
+}
+
+export default {
+  add_company_to_crm__f24aad6b
+}
