334c61ebf103c092ae14c4845c431be1758b7559
admin@just-ai.com <admin@just-ai.com>
2026-03-27 09:49:15 +0000

    Apply template: ai-recruiter-ru

--- src/collections/Amo_Crm/Amo_Crm.js
+++
@@ -1,8 +0,0 @@
-async function add_company_to_crm__f24aad6b(params) {
-    params.name = "null"
-    return await Amo_Crm.add_company_to_crm(params)
-}
-
-export default {
-  add_company_to_crm__f24aad6b
-}
